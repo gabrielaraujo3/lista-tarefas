9ad0c26a039a7272bec5568b249d98e659de384b
Gabriel <araujog388@gmail.com>
2023-01-08 18:49:55 -0300

    Aula react básico 08 apagando tarefas

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'react/state-in-constructor': 0,
     'import/no-duplicates': 0,
-
+    'class-methods-use-this': 0,
+    'no-console': 0,
   },
 };
